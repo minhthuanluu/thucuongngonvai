e280deb33873c5f3f784809ed9f95ef2a6c59e7d
Thy96 <caodinhthy1996@gmail.com>
2022-09-30 14:54:35 +0700

    commit 2

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -8,7 +8,7 @@ function Home({ items, handleAdd }) {
         <div id="A"></div>
         <div className="home-hot-products">
           <div className="section-title">
-            <div className="text-1 text-2">Su Menu lan 3333333</div>
+            <div className="text-1 text-2">Su Menu</div>
           </div>
           <div className="section-content container">
             <div className="list-product">
